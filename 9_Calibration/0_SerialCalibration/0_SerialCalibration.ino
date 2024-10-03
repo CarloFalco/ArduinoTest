3c02a0d56415622ade99e03e6d5f13a4b9356b6e
Carlo <48788661+CarloFalco@users.noreply.github.com>
2024-10-03 22:06:47 +0200

    move calibration function under define

--- 9_Calibration/0_SerialCalibration/0_SerialCalibration.ino
+++ 9_Calibration/0_SerialCalibration/0_SerialCalibration.ino
@@ -5,6 +5,8 @@
 #define TASK_CALIB   100                // 0.1ms
 #define ENABLE_CALIBRATION
 
+#define LED_BUILTIN 97
+
 typedef struct {
     String nome;
     int valore;
@@ -23,6 +25,8 @@ void setup() {
     Serial.begin(115200); // Inizializza la comunicazione seriale a 9600 baud
     Serial.println("Usa 'put nome valore' per aggiornare una variabile o 'get nome' per chiedere il valore attuale:");
 
+    pinMode(LED_BUILTIN, OUTPUT);
+
     // Crea i task
     Scheduler_PwrOn();
 }
@@ -34,6 +38,8 @@ void loop() {
 }
 
 
+
+#ifdef ENABLE_CALIBRATION
 
 void gestisciSeriale() {
     if (Serial.available() > 0) { // Controlla se ci sono dati disponibili
@@ -96,11 +102,27 @@ void task_calib(void* pvParameters) {
   }
 }
 
+#endif
+
+
+void task_blink(void* pvParameters) {  
+  TickType_t lastWakeTime = xTaskGetTickCount();
+  const TickType_t period = pdMS_TO_TICKS(1000);
+
+  while (true) {
+    // Esegui il codice del task 1
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
+    vTaskDelayUntil(&lastWakeTime, period);
+  }
+}
+
 
 
 // Creazione dei task //
 void Scheduler_PwrOn(void) {
 
+  #ifdef ENABLE_CALIBRATION
 
   xTaskCreatePinnedToCore(
     task_calib,         // Task function.
@@ -110,7 +132,16 @@ void Scheduler_PwrOn(void) {
     1,             // priority of the task
     &task1Handle,  // Task handle to keep track of created task
     0);            // pin task to core 0        // pin task to core 0    
+  #endif
 
+  xTaskCreatePinnedToCore(
+  task_blink,         // Task function.
+  "task_blink",       // name of task.
+  2048,          // Stack size of task
+  NULL,          // parameter of the task
+  1,             // priority of the task
+  &task1Handle,  // Task handle to keep track of created task
+  0);            // pin task to core 0        // pin task to core 0   
 }
 
 
