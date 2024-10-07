1b7abf2123a2918cc4d41bc39cd9369ed6f402b1
Carlo <48788661+CarloFalco@users.noreply.github.com>
2024-10-07 22:46:10 +0200

    Add EEPROM variable and function

--- 4_FreeRTOS_RTC_WIFI/Utilities.cpp
+++ 4_FreeRTOS_RTC_WIFI/Utilities.cpp
@@ -1,6 +1,18 @@
 #include <ESP32Time.h>
 #include "Utilities.h"
 
+/*
+Eeprom_Data_Type eepromData = {
+      String(""),
+      { String("CHAT_ID_1"), String("CHAT_ID_2"), String(""), String("") },
+      { 1, 0, 0, 0 },
+      false,                // Boolean can bee true or false, or LOW or HIGH
+      false,
+      LOW, 
+      10,      
+      14   
+};
+*/
 
 void setup_rtc_time(ESP32Time *rtc){
     // Configurazione del fuso orario
@@ -70,3 +82,41 @@ void check_DST(ESP32Time *rtc) {
 
 
 
+// FUNZIONI RELATIVE ALLA EEPROM
+void do_eprom_read() {
+
+  EEPROM.begin(200);
+  EEPROM.get(0, eepromData);
+
+  Serial.print("EEPROM STS_Before: ");
+  Serial.println(eepromData.eprom_good);
+
+  if (eepromData.eprom_good == 14) {
+    Serial.println("Good settings in the EPROM ");
+    Serial.print("EEPROM STS: ");
+    Serial.println(eepromData.eprom_good);
+    // eepromData.Eeprom_bArray[i] // go access to information inside the struct
+
+  } else {
+    Serial.println("EPROM canot be read ");
+    eepromData = {
+      String(""),
+      { String("CHAT_ID_1"), String("CHAT_ID_2"), String(""), String("") },
+      { 1, 0, 0, 0 },
+      false,                // Boolean can bee true or false, or LOW or HIGH
+      false,
+      LOW, 
+      10,      
+      14   
+    };
+    do_eprom_write();
+  }
+}
+
+void do_eprom_write() {
+  Serial.println("Writing to EPROM ...");
+  EEPROM.begin(200);
+  EEPROM.put(0, eepromData);
+  EEPROM.commit();
+  EEPROM.end();
+}
